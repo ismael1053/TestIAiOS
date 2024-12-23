31d1e34eab979e9d9a4114c2daad837120dfe76f
Roque Sandoval Serrato <roque.sandoval@rappi.com>
2024-12-23 13:32:05 -0600

    Initial Commit

---
+++ TestIA/TestIAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestIAApp.swift
+//  TestIA
+//
+//  Created by Roque Sandoval Serrato on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TestIAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
